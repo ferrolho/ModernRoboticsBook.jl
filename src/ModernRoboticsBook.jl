61e5caaaa3e0a2c529edfb851aa5b3f33308a27e
Henrique Ferrolho <henriqueferrolho@gmail.com>
2019-02-26 16:27:36 +0000

    Fixes python not to !

--- src/ModernRoboticsBook.jl
+++ src/ModernRoboticsBook.jl
@@ -199,9 +199,9 @@ function MatrixLog3(R::Array)
     if acosinput >= 1
         return zeros(3, 3)
     elseif acosinput <= -1
-        if not NearZero(1 + R[3, 3])
+        if !NearZero(1 + R[3, 3])
             omg = (1.0 / √(2 * (1 + R[3, 3]))) * [R[1, 3], R[2, 3], 1 + R[3, 3]]
-        elseif not NearZero(1 + R[2, 2])
+        elseif !NearZero(1 + R[2, 2])
             omg = (1.0 / √(2 * (1 + R[2, 2]))) * [R[1, 2], 1 + R[2, 2], R[3, 2]]
         else
             omg = (1.0 / √(2 * (1 + R[1, 1]))) * [1 + R[1, 1], R[2, 1], R[3, 1]]
